2d41e17b289b934d33a2421bbdc7653072212efa
otacilioruas <otacilioruas@gmail.com>
2016-10-18 14:20:16 -0300

    Update teste1.py

--- teste1.py
+++ teste1.py
@@ -1,14 +1,15 @@
 #-*- coding:UTF-8 -*-
 from Tkinter import *
 
-class novo:
+root = Tk()
 
+class novo:
     def __init__(self, janela):
         # Inicia como None
         self.jan = None
         self.caixa=Frame(janela)
         self.caixa.grid()
-        self.b=Button(janela, text='Cadastrar', width=10, command=self.new_jan)
+        self.b=Button(janela, text='Cadastrar', width=10, command=self.cadastro)
         self.b.grid()
         self.b.place(x=30, y=50)
         self.b=Button(janela, text='Saldo', width=10, command=self.saldo)
@@ -23,11 +24,11 @@ class novo:
         self.b=Button(janela, text='Extrato', width=10, command=self.new_jan)
         self.b.grid()
         self.b.place(x=30, y=170)
-        self.b=Button(janela, text='Sair', width=10, command=self.new_jan)
+        self.b=Button(janela, text='Sair', width=10, command=self.close)
         self.b.grid()
         self.b.place(x=30, y=200)
-        self.l1=Label(janela, text='raiz!')
-        self.l1.grid()
+        #self.l1=Label(janela, text='raiz!')
+        #self.l1.grid()
 
     def new_jan(self):
         # Verifica se já foi criada
@@ -37,12 +38,27 @@ class novo:
             self.l=Label(self.jan, text='apenas fechando essa janela poderá voltar ou clicar na raiz!')
             self.l.grid()
             self.jan.geometry('300x200')
+
+    def cadastro(self):
+        if self.jan is None:
+            self.jan=Tk()
+            self.mente = StringVar()
+            self.b=Button(self.jan, text='Enviar', width=10, command=self.fecha_jan)
+            self.b.grid()
+            self.b.place(x=100, y=100)
+            self.entrada = Entry(self.jan,  textvariable=self.mente).place()
+            self.jan.protocol("WM_DELETE_WINDOW", self.fecha_jan)
+            self.jan.geometry('300x200')
     def saldo(self):
         if self.jan is None:
             self.jan=Tk()
+            self.b=Button(self.jan, text='voltar', width=10, command=self.fecha_jan)
+            self.b.grid()
+            self.b.place(x=100, y=100)
             self.jan.protocol("WM_DELETE_WINDOW", self.fecha_jan)
             self.l=Label(self.jan, text='Saldo R$ 0,00')
             self.l.grid()
+            self.l.place(x=100, y=70)
             self.jan.geometry('300x200')
         else:
             # Se já foi, basta colocá-la na frente
@@ -53,10 +69,11 @@ class novo:
         self.jan.destroy()
         self.jan = None
 
-root=Tk()
+    def close(self):
+        global root
+        root.destroy()
 
 novo(root)
 root.geometry('400x300')
-
+# root.protocol("WM_DELETE_WINDOW", close)
 root.mainloop()
-novo()
